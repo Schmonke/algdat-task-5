ee159aa15bf7f16b49e0dc279f9911e7abc3ff5c
BabyPresident <mag.steensland@gmail.com>
2021-09-26 23:30:52 +0200

    created basic skeleton for texthashing from file

---
+++ makefile
@@ -0,0 +1,2 @@
+make:
+	gcc texthashtable.c -o texthashtable && gcc hashperformance.c -o hashperformance
---
+++ texthashtable.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+// Create hash function that converts text keys to integers
+// in the range [0, m-1] where m is the length of the array.
+
+// "Lastfaktor" for an array with size m and n elements, is 
+// given as a = n / m.
+
+
+
+int convert_text_to_int(char* text)
+{
+    int length = strlen(text);
+    int result = 0;
+
+    for(int i = 0; i < length; i++)
+    {
+        //subtracting char A and adding 1 gives ASCII int value of char
+        result += text[i] - 'A' + 1; 
+    }
+
+    return result;
+}
+
+// Temporary hashing func
+int hashfunc(int key, int tablesize) 
+{
+    return key % tablesize;
+}
+
+int run_with_file(FILE *fp)
+{
+    int const buffer_size = 255;
+    char buff[buffer_size];
+    int text_len = 1;
+    int m = 42069;
+    char table[m];
+    int n, a;
+
+    while(text_len >= 0) 
+    {
+        text_len = getline(buff[0], buffer_size, fp);
+        n = convert_text_to_int(buff[0]);
+        a = hashfunc(n, m);
+        table[a] = buff[0];
+    }
+
+    return 0;
+}
+
+int handle_file(char const *argv[])
+{
+    char *file_path = argv[1];
+    FILE *file = fopen(file_path, "r");
+
+    if(file == NULL) 
+    {
+        perror("Unable to open file.\n");
+        return 1;
+    }
+    return run_with_file(file);
+}
+
+void print_help()
+{
+    printf(
+        "You must specify which file to read from as an argument to the program.\n"
+        "Usage: texthashtable <file_name>\n"
+        "E.g. ./texthashtable ~/main.c\n"
+    );
+}
+
+int main(int argc, char const *argv[])
+{
+    if (argc < 2) 
+    {
+        print_help();
+        return 1;
+    }
+    handle_file(argv[1]);
+}
