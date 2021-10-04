906aa20b60e5d61161bdc4db057a60119fe3c8f0
norgor <7034394+norgor@users.noreply.github.com>
2021-10-04 11:47:21 +0200

    improved hashing algorithms

--- hashperformance.c
+++ hashperformance.c
@@ -6,22 +6,40 @@
 #include <time.h>
 #include <math.h>
 
+typedef struct hash_context
+{
+    unsigned long long mult_A;
+    unsigned int capacity_pow2exp;
+    unsigned int capacity;
+} hash_context;
+
+typedef struct probe_context
+{
+    hash_context *hash_ctx;
+    int key;
+    unsigned int hash1;
+    unsigned int hash2;
+    size_t capacity;
+} probe_context;
+
+typedef size_t probe_func(probe_context *ctx, int i);
+
 typedef struct
 {
     bool exists;
     int value;
 } hash_table_entry;
 
-typedef size_t probe_func(int a, int b, int key, size_t hash_table_capacity);
-
 typedef struct
 {
+    size_t capacity_pow2exp;
     size_t capacity;
     size_t entries;
     size_t collisions;
     hash_table_entry *values;
 
     probe_func *probe;
+    hash_context hash_ctx;
 } hash_table;
 
 /**
@@ -34,7 +52,6 @@ size_t lrot(size_t value, size_t bits)
 {
     return (value << bits) | (value >> (sizeof(value) * CHAR_BIT) - bits);
 }
-
 
 void swap(int *a, int *b)
 {
@@ -59,7 +76,7 @@ int *create_random_unique_array(size_t length)
     return array;
 }
 
-size_t power_of_two_round(size_t value)
+size_t pow2_round_exponent(size_t value)
 {
     const int bits = sizeof(size_t) * CHAR_BIT;
     if (value & 1 << (bits - 1))
@@ -68,69 +85,102 @@ size_t power_of_two_round(size_t value)
     }
     for (int i = bits - 2; i > 0; i--)
     {
-        if (value & 1 << i)
+        if (value & (size_t)1 << i)
         {
-            return 1 << i + 1;
+            return (i + 1);
         }
     }
-    return 1;
+    return 0;
 }
 
-//Calculate loadfactor
-float get_loadfactor(hash_table *table)
+size_t pow2_round(size_t value)
 {
-    return ((float) table->entries/ (float) table->capacity)*100;
+    return (size_t)1 << pow2_round_exponent(value);
+}
+
+//Calculate load factor
+float get_load_factor(hash_table *table)
+{
+    return ((float)table->entries / (float)table->capacity) * 100;
+}
+
+void hash_context_init(hash_table *table)
+{
+    const double sqrt5 = 2.236067977;
+    table->hash_ctx = (hash_context){
+        .mult_A = (unsigned long long)(0.5 * table->capacity * (sqrt5 - 1)),
+        .capacity_pow2exp = table->capacity_pow2exp,
+        .capacity = table->capacity,
+    };
 }
 
 //Hashfunc 1
-unsigned int hash1(int key, int m)
+unsigned int hash1(hash_context ctx, int key)
 {
-    const int n = 16;
-    const unsigned long long A = (unsigned long long)(0.5*m*(sqrt(5)-1));
-    return key * A >> (32 - n); //x is from 2^x
+    // multiplicative hash implementation
+    return key * ctx.mult_A >> (sizeof(int) * CHAR_BIT - ctx.capacity_pow2exp);
 }
 
 //Hashfunc 2
-unsigned int hash2(int key, int m)
-{   
-    /*const int a = 1093847;
-    const int n = 16;
-    return (key*a>>(31 - n)) & (m-1);*/
-    const float n = 0.5;
-    return floor (m * ( key/2)) + 1;
+unsigned int hash2(hash_context ctx, int key)
+{
+    // folding hash implementation
+    unsigned int mask = ctx.capacity - 1;
+    unsigned int h = 0;
+    unsigned int shift = 0;
+    while (mask != 0)
+    {
+        h = (mask & key) >> shift;
+        mask <<= ctx.capacity_pow2exp;
+        shift += ctx.capacity_pow2exp;
+    }
+
+    return h | 1;
 }
 
-size_t probe_linear(int h, int _, int i, size_t m)
+size_t probe_linear(probe_context *ctx, int i)
 {
-    return (h + i) % m;
+    return (ctx->hash1 + i) % ctx->capacity;
 }
 
 //Uses prime numbers as constants to minimize chance of common denominator.
-size_t probe_quadratic(int h, int _, int i, size_t m)
+size_t probe_quadratic(probe_context *ctx, int i)
 {
-    const int c = 2;   
+    const int c = 2;
     const int ic = (i / c);
-    size_t result = (h + ic + ic*i);
     // equivalent to (h + i*0.5 + 0.5*i^2)
-    return (h + ic + ic*i) % m;
+    size_t result = (ctx->hash1 + ic + ic * i);
+    return result % ctx->capacity;
 }
 
 // h2 and m must be relative prime
-size_t probe_doublehash(int h1, int h2, int i, size_t m)
+size_t probe_doublehash(probe_context *ctx, int i) //int h1, int h2, int i, size_t m)
 {
-    return (h1 + i * h2) % m;
+    // hash2 cannot be 0 as it will cancel multiplication by i
+    if (ctx->hash2 == 0)
+    {
+        ctx->hash2 = hash2(*ctx->hash_ctx, ctx->key);
+    }
+    return (ctx->hash1 + i * ctx->hash2) % ctx->capacity;
 }
 
-int hash_table_add(hash_table *table, int v) 
+size_t hash_table_add(hash_table *table, int v)
 {
     const size_t capacity = table->capacity;
-    const int h1 = hash1(v, capacity);
-    const int h2 = hash2(v, capacity);
-    int colls = 0;
+    hash_context *hash_ctx = &table->hash_ctx;
+    probe_context ctx = (probe_context){
+        .hash_ctx = hash_ctx,
+        .key = v,
+        .capacity = table->capacity,
+        .hash1 = hash1(*hash_ctx, v),
+        .hash2 = 0,
+    };
+
+    size_t colls = 0;
 
     for (int i = 0; i < capacity; i++)
     {
-        int j = table->probe(h1, h2, i, capacity);
+        int j = table->probe(&ctx, i);
         hash_table_entry *value = &table->values[j];
         if (!value->exists)
         {
@@ -148,9 +198,11 @@ int hash_table_add(hash_table *table, int v)
 hash_table *hash_table_create(size_t min_capacity, probe_func *probe)
 {
     hash_table *table = calloc(1, sizeof(hash_table));
-    table->capacity = power_of_two_round(min_capacity);
+    table->capacity_pow2exp = pow2_round_exponent(min_capacity);
+    table->capacity = pow2_round(min_capacity);
     table->values = calloc(table->capacity, sizeof(hash_table_entry));
     table->probe = probe;
+    hash_context_init(table);
     return table;
 }
 
@@ -180,19 +232,18 @@ int hash_table_add_all(hash_table *table, int *values, size_t values_length)
 
 int main(int argc, char *argv[])
 {
-    const struct 
+    const struct
     {
         const char *name;
         probe_func *probe;
     } probe_types[] = {
-        { "linear", &probe_linear },
-        { "quadratic", &probe_quadratic },
-        { "double-hash", &probe_doublehash }
-    };
-    const int probe_types_length = sizeof(probe_types)/sizeof(probe_types[0]);
-    
-    const float fill_ratios[] = { 0.5, 0.8, 0.9, 0.99, 1.0 };
-    const int fill_ratios_length = sizeof(fill_ratios)/sizeof(fill_ratios[0]);
+        {"linear", &probe_linear},
+        {"quadratic", &probe_quadratic},
+        {"double-hash", &probe_doublehash}};
+    const int probe_types_length = sizeof(probe_types) / sizeof(probe_types[0]);
+
+    const float fill_ratios[] = {0.5, 0.8, 0.9, 0.99, 1.0};
+    const int fill_ratios_length = sizeof(fill_ratios) / sizeof(fill_ratios[0]);
 
     int table_bound = 10000000;
 
@@ -203,7 +254,7 @@ int main(int argc, char *argv[])
 
     srand(time(NULL));
 
-    int table_size = power_of_two_round(table_bound);
+    int table_size = pow2_round(table_bound);
 
     printf("Generating %d unique numbers...\n\n", table_size);
     int *rand_array = create_random_unique_array(table_size);
@@ -216,26 +267,26 @@ int main(int argc, char *argv[])
             float fill_ratio = fill_ratios[j];
             printf("Creating table for %s\n", probe_types[i].name);
             hash_table *table = hash_table_create(table_bound, probe_types[i].probe);
-            printf("Filling table (%02.0f%%) ...\n", fill_ratio*100);
+            printf("Filling table (%02.0f%%) ...\n", fill_ratio * 100);
 
-            if(clock_gettime(CLOCK_REALTIME, &start))
+            if (clock_gettime(CLOCK_REALTIME, &start))
             {
                 printf("Time failure");
                 return -1;
             }
             int col = hash_table_add_all(table, rand_array, table_size * fill_ratio);
-        
-            if(clock_gettime(CLOCK_REALTIME, &end))
+
+            if (clock_gettime(CLOCK_REALTIME, &end))
             {
                 printf("Time failure");
                 return -1;
             }
-            
-            printf("Time       : %.3fms\n", (end.tv_sec-start.tv_sec)*1000.0 + (end.tv_nsec - start.tv_nsec) / 1000000.0);
-            printf("Capacity   : %ld\n", table->capacity);  
+
+            printf("Time       : %.3fms\n", (end.tv_sec - start.tv_sec) * 1000.0 + (end.tv_nsec - start.tv_nsec) / 1000000.0);
+            printf("Capacity   : %ld\n", table->capacity);
             printf("Collisions : %d\n", col);
-            printf("Loadfactor : %.0f%%\n", get_loadfactor(table));
-            printf("Entries    : %ld\n",table->entries );
+            printf("Loadfactor : %.0f%%\n", get_load_factor(table));
+            printf("Entries    : %ld\n", table->entries);
             printf("Freeing table...\n\n");
             hash_table_free(table);
         }
